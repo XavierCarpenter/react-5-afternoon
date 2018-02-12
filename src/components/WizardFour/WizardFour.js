3d726764afc9b13eaec3d3e6955bd49b5bd62be4
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-02-12 17:13:11 -0600

    completed

--- src/components/WizardFour/WizardFour.js
+++ src/components/WizardFour/WizardFour.js
@@ -1,21 +1,36 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import updateFound from  '../../ducks/reducer.js';
 
 class WizardFour extends Component {
     render(){
-        return(
-            <div className="parent-div">
-                <div className="vert-align">            
-                    <p>Have you already found your new home?</p> <br />
-                    
-                    <div className="row">
-                        <Link to="/wFive"><button onClick={this.props.foundTrue}>Yes</button></Link>
-                        <Link to="/wFive"><button onClick={this.props.foundFalse}>No </button></Link>  
-                    </div>         
-                </div>
+        return <div className="parent-div">
+            <div className="vert-align">
+              <p>Have you already found your new home?</p> <br />
+              <div className="row">
+                <Link to="/wFive">
+                  <button onClick={e => this.props.updateFound("True")}>
+                    Yes
+                  </button>
+                </Link>
+                <Link to="/wFive">
+                  <button onClick={e => this.props.updateFound("False")}>
+                    No{" "}
+                  </button>
+                </Link>
+              </div>
             </div>
-        )
+          </div>;
     }
 }
 
-export default WizardFour;
+const mapStateToProps = state => {
+    const { found } = state;
+
+    return {
+        found
+    };
+}
+
+export default connect(mapStateToProps, {updateFound} ) (WizardFour);
